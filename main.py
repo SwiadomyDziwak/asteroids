4ff065a44161f6d01a4b566f114c8127f86fc402
SwiadomyDziwak <kojack26@gmail.com>
2024-12-14 22:24:17 +0100

    added player shape and fps limit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pygame
 from constants import *
+from player import Player
+
+#source venv/bin/activate
 
 def main():
     pygame.init()
@@ -7,12 +10,22 @@ def main():
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2, radius = PLAYER_RADIUS)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color=(0,0,0))
+
+        player.draw(screen)
+
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
